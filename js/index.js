0e3dd8300863385b1e0c17b15111fb7fecf35396
Cris Morita <cristina_tiemi.social@yahoo.com>
2024-12-28 15:37:31 -0300

    getting Html elements

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 "use strict";
+const form = document.querySelector('#form');
+const input = document.querySelector('#location-form');
+const button = document.querySelector('#location-button');
+const data = document.querySelector('#wheather-data');
+const info = document.querySelector('.wheather-info');
+form === null || form === void 0 ? void 0 : form.addEventListener('submit', (event) => {
+    event.preventDefault();
+});
---
+++ ts/index.ts
@@ -0,0 +1,10 @@
+const form = document.querySelector('#form')
+const input = document.querySelector('#location-form')
+const button = document.querySelector('#location-button')
+const data = document.querySelector('#wheather-data')
+const info = document.querySelector('.wheather-info')
+
+form?.addEventListener('submit', (event) => {
+    event.preventDefault()
+    
+})
